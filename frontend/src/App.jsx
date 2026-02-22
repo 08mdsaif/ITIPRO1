c3e9503c8121df2efeff86e2f059f932a33e4024
08mdsaif <mdsaifop1@gmail.com>
2026-02-22 12:43:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 // import logo from './assets/itipro-logo.svg';
 import logo from './assets/logo.jpeg';
 
-const API_URL = 'https://itipro-1.vercel.app/';
+const API_URL = 'https://itipro-1.vercel.app/api';
 
 const successSteps = [
   'Learn technology and build your profile.',
